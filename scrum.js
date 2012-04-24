a70ccefda6a67b918da551d91a4495ac81a117c5
Juan Pujol <juan@Juan-iMac.local>
2012-04-23 21:08:22 -0300

    first commit

---
+++ scrum.js
@@ -0,0 +1,65 @@
+Stories = new Meteor.Collection("stories");
+Todos = new Meteor.Collection("todos");
+
+if(Meteor.is_client){
+  
+  Template.stories_list.stories = function(){
+    return Stories.find({}, {sort: {points: -1}});
+  };
+
+  Template.new_story.events = {
+    'click button': function(){
+      if($("input[name='title']").val() !== ""){
+        Stories.insert({
+          title: $("input[name='title']").val(),
+          points: $("input[name='points']").val()
+        });
+        return $("input").val("");
+      }
+    },
+    'keydown': function(event){
+      if (event.keyCode === 13 && $("input[name='title']").val() !== "") {
+        event.preventDefault();
+        Stories.insert({
+          title: $("input[name='title']").val(),
+          points: $("input[name='points']").val()
+        });
+        return $("input").val("");
+      }
+    }
+  };
+
+  Template.todos_list.todos = function(){
+    return Todos.find({story_id:this._id, status:"todo"});
+  };
+
+  Template.wip_list.todos = function(){
+    return Todos.find({story_id:this._id, status:"wip"})
+  };
+
+  Template.verify_list.todos = function(){
+    return Todos.find({story_id:this._id, status:"verify"})
+  };
+
+  Template.done_list.todos = function(){
+    return Todos.find({story_id:this._id, status:"done"})
+  };
+
+  Template.new_todo.events = {
+    'click button':function(){
+      if($("textarea").val() !== ""){
+        Todos.insert({
+          text: $("textarea").val(),
+          status: "todo",
+          story_id: this._id
+        });
+        return $("textarea").val("");
+      };
+    },
+    'click button.cross':function(event){
+      // TODO - Fazer funcionalidade para mostrar e esconder form.
+      console.log("Show me the form");
+    }
+  };
+
+};
